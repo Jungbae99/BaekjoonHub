00647ffdbfb7e08cbedecfc125cc5eaf30f0e798
Jungbae99 <116333199+Jungbae99@users.noreply.github.com>
2023-05-25 21:28:44 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[]arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if(sum - arr[i] - arr[j] == 100){
+                    for (int k = 0; k < 9; k++) {
+                        if(k==i || k==j) continue;
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+
+    }
+}
