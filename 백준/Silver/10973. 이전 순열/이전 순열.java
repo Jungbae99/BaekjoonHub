a4ddef32b0e479ff06a2965495206b4a3ddf4fd7
Jungbae99 <116333199+Jungbae99@users.noreply.github.com>
2023-06-08 17:24:05 +0900

    [Silver III] Title: 이전 순열, Time: 676 ms, Memory: 30436 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int arr[];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        if (nextpermulation()) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }else{
+            System.out.println(-1);
+        }
+    }
+
+    public static boolean nextpermulation() {
+        int i = n-1;
+        while (i > 0 && arr[i] > arr[i - 1]) i -= 1;
+        if (i <= 0) return false;
+
+        int j = n-1;
+        while(arr[i-1] < arr[j]) j-= 1;
+
+        swap(i-1, j);
+        j = n - 1;
+        while (j > i) {
+            swap(j,i);
+            j--; i++;
+        }
+        return true;
+    }
+
+    public static void swap(int j, int i) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
