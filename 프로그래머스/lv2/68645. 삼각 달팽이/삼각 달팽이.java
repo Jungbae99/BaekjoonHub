8f3e0f69f7cf3c0d16d626bca019593fd0369574
전정배 <116333199+Jungbae99@users.noreply.github.com>
2023-09-17 14:56:26 +0900

    [level 2] Title: 삼각 달팽이, Time: 14.21 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] solution(int n) {
+         int[] answer = new int[n * (n + 1) / 2];
+        int[][] tri = new int[n][n];
+
+        int x = -1;
+        int y = 0;
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i % 3 == 0) {
+                    x++;
+                } else if (i % 3 == 1) {
+                    y++;
+                } else if (i % 3 == 2) {
+                    x--;
+                    y--;
+                }
+                tri[x][y] = num++;
+            }
+        }
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tri[i][j] == 0) break;
+                answer[index++] = tri[i][j];
+            }
+        }
+        return answer;
+    }
+}
