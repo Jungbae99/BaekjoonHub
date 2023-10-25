e158a3b31aadbe64d571efc7c51c3898ec716e59
전정배 <116333199+Jungbae99@users.noreply.github.com>
2023-10-25 12:41:57 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+select animal_id, name, sex_upon_intake 
+
+from animal_ins
+
+where name = 'ella' or 
+name = 'Lucy' or 
+name = 'Pickle' or 
+name = 'Rogan' or 
+name = 'Sabrina' or 
+name = 'Mitty'
+
+order by animal_id;
+
