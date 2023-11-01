ed2cab00144faaf15eac7dae091f9350285b58f6
전정배 <116333199+Jungbae99@users.noreply.github.com>
2023-11-01 23:38:43 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+select ai.animal_id, ai.animal_type, ai.name
+from animal_ins ai 
+join animal_outs ao on ai.animal_id = ao.animal_id
+and ai.sex_upon_intake <> ao.sex_upon_outcome
+order by ai.animal_id;
