ad1accd5f20800d1591f6c68753df2af9da9d711
전정배 <116333199+Jungbae99@users.noreply.github.com>
2024-03-21 23:52:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 208 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isPrime = true;
+
+            int num = sc.nextInt();
+
+            if (num == 1) {
+                continue;
+            }
+
+            for (int j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
