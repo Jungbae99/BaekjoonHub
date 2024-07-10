ac55e6a06f81df77f994f7fe776a4784d550eee1
전정배 <116333199+Jungbae99@users.noreply.github.com>
2024-07-11 00:15:32 +0900

    [level 2] Title: 더 맵게, Time: 1754.22 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+        
+        while (pq.peek() < K) {
+            if (pq.size() < 2) {
+                return -1;
+            }
+            pq.add(pq.poll() + pq.poll() * 2);
+            answer ++;
+        }
+        return answer;
+    }
+}
