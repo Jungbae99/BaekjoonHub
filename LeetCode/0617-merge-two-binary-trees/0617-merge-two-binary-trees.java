ba75460859c13a4b04b74b904886052f983574f0
전정배 <116333199+Jungbae99@users.noreply.github.com>
2024-09-25 22:30:30 +0900

    Time: 0 ms (100%), Space: 44.8 MB (67.09%) - LeetHub

---
+++ LeetCode/0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        TreeNode newNode = new TreeNode(root1.val + root2.val);
+
+        newNode.left = mergeTrees(root1.left, root2.left);
+        newNode.right = mergeTrees(root1.right, root2.right);
+        return newNode;
+    }
+}
