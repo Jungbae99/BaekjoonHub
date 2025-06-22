686d67d9fb24c6ceb52fff9afe28f82e53fa7b4f
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-06-22 23:26:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,18 +1,25 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
 
-        int[] d = new int[1001];
-        d[1] = 1;
-        d[2] = 2;
-        for (int i = 3; i <= n; i++)
-            d[i] = (d[i - 1] + d[i - 2]) % 10007;
+        System.out.println(tiling(i));
+    }
+ private static long tiling(int n) {
+        long[] arr = new long[n + 1];
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 3;
+     
+        arr[2] = 2;
+        arr[3] = 3;
 
-        System.out.println(d[n]);
+        for (int i = 4; i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+        }
 
+        return arr[n];
     }
 }
