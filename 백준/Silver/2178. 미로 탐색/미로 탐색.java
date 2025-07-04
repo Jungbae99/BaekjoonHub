97dfc27c710d262ffac48ee4e8a4751b9ebf6a4b
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-07-04 16:54:01 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,69 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
-    int x, y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int arr[][] = new int[n][m];
-        boolean visited[][] = new boolean[n][m];
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        int count = 0;
-
-        Queue<Node> queue = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = s.charAt(j) - '0';
+        int[][] arr = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                int val = line.charAt(j - 1) - '0';
+                arr[i][j] = val;
             }
         }
 
-        Node start = new Node(0, 0);
-        queue.add(start);
-        visited[0][0] = true;
+        System.out.println(bfs(arr));
+    }
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    private static int bfs(int[][] arr) {
+        Queue<Node> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[arr.length][arr[0].length];
+        queue.add(new Node(1, 1, 1));
+        visited[1][1] = true;
 
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            for (int k = 0; k < 4; k++) {
+            Node now = queue.poll();
 
-                int newX = dx[k] + node.x;
-                int newY = dy[k] + node.y;
+            if (now.x == arr.length - 1 && now.y == arr[0].length - 1) {
+                return now.val;
+            }
 
+            for (int i = 0; i < 4; i++) {
+                int nextX = now.x + dx[i];
+                int nextY = now.y + dy[i];
 
-                if (newX < 0 || newX >= n || newY < 0 || newY >= m) {
-                    continue;
-                }
-
-                if (visited[newX][newY] || arr[newX][newY] == 0) {
-                    continue;
-                }
-
-                queue.add(new Node(newX, newY));
-                arr[newX][newY] = arr[node.x][node.y] + 1;
-                visited[newX][newY] = true;
+                if (nextX < 0 || nextY < 0 || nextX >= arr.length || nextY >= arr[0].length || visited[nextX][nextY]) continue;
+                if (arr[nextX][nextY] == 0) continue;
+                visited[nextX][nextY] = true;
+                queue.add(new Node(nextX, nextY, now.val + 1));
             }
         }
-        System.out.println(arr[n - 1][m - 1]);
 
+        return -1;
+    }
+
+
+
+    static class Node {
+        int x, y, val;
+
+        private Node(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
     }
 }
