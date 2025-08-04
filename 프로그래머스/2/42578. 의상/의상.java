caba84122e82410b6d06817fd560251dffaf1e7a
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-08-04 23:58:48 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) +1 );
+        }
+
+        int answer = 1;
+        for (Integer value : map.values()) {
+            answer *= (value + 1);
+        }
+
+        return answer -1;
+    }
+}
