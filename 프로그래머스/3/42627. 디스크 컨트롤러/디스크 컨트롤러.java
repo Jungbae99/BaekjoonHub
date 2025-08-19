0f76c98d952e398d32e999f89fdcf0b8fba6a706
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-08-20 00:31:15 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.23 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,37 +1,55 @@
 import java.util.*;
 
 class Solution {
+    static class Node implements Comparable<Node> {
+        int index, arriveTime, time;
+
+        public Node(int index, int arriveTime, int time) {
+            this.index = index;
+            this.arriveTime = arriveTime;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.time != o.time) return this.time - o.time;
+            if (this.arriveTime != o.arriveTime) return this.arriveTime - o.arriveTime;
+            return this.index - o.index;
+        };
+    }
+
     public int solution(int[][] jobs) {
         int answer = 0;
-        
-        Arrays.sort(jobs, (o1, o2) -> {
-            return o1[0] - o2[0];
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        Arrays.sort(jobs, (int[] o1, int[] o2) -> {
+                return o1[0] - o2[0];
         });
-        
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        
-        int pqIndex = 0;
-        int end = 0;
-        int count = 0;
-        
-        while( count < jobs.length ){
-            
-            while(pqIndex < jobs.length  && jobs[pqIndex][0] <= end ){
-                pq.add(jobs[pqIndex++]);
+
+        int idx = 0;        // 아직 큐에 넣지 않은 작업의 인덱스
+        int time = 0;       // 현재 시간(완료 시각 진행)
+        int done = 0;       // 완료한 작업 수
+        int totalTurn = 0;  // 반환 시간 합
+        int n = jobs.length;
+
+        while (done < n) {
+            while (idx < n && jobs[idx][0] <= time) {
+                pq.add(new Node(idx, jobs[idx][0], jobs[idx][1]));
+                idx++;
             }
-            
-            if(pq.isEmpty()){
-                end = jobs[pqIndex][0];
-            } else {
-                
-                int[] tmp  = pq.poll();
-                answer += tmp[1] - tmp[0] + end;
-                end += tmp[1];
-                count++;
+
+            if (pq.isEmpty()) {
+                // 대기 작업이 없으면 시간을 다음 도착 시각으로 점프
+                time = jobs[idx][0];
+                continue;
             }
+
+            // 가장 짧은 작업 수행
+            Node cur = pq.poll();
+            time += cur.time;                         // 이 작업의 완료 시각
+            totalTurn += (time - cur.arriveTime);     // 반환 시간 = 완료 - 도착
+            done++;
         }
-            
-            
-        return answer / count;
+
+        return totalTurn / n; // 정수부분
     }
 }
