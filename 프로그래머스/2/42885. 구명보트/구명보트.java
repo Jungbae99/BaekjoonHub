95ad9e6a4d32007d84d98c0f41733afe123fd1c2
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-08-31 23:48:35 +0900

    [level 2] Title: 구명보트, Time: 11.52 ms, Memory: 57.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        int count = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
+
