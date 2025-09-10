7f59aeff6bafa147a5ec66cd1897ed2b9edfbce4
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-09-11 00:19:43 +0900

    [level 3] Title: 단속카메라, Time: 7.88 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,23 +1,20 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
-
-        Arrays.sort(routes, (o1, o2) -> {
-            return o1[1] - o2[1];
-        });
-
-        int cam = Integer.MIN_VALUE;
-        for (int[] route : routes) {
-
-
-            if (route[0] > cam) {
-                cam = route[1];
-                answer++;
+      Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int count = 1;
+        int lastCamera = routes[0][1]; // 첫 번째 구간의 끝점에 카메라 설치
+        
+        for (int i = 1; i < routes.length; i++) {
+            // 현재 카메라가 이 구간을 커버하지 못하면
+            if (routes[i][0] > lastCamera) {
+                count++;
+                lastCamera = routes[i][1]; // 새 구간의 끝점에 카메라 설치
             }
         }
-
-        return answer;
+        
+        return count;
     }
 }
