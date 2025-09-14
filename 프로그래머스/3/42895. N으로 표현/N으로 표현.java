556285e73631c4e33005bc285c72aef24e6c28b6
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-09-15 00:10:10 +0900

    [level 3] Title: N으로 표현, Time: 55.79 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,36 +1,49 @@
 import java.util.*;
 
 class Solution {
-    public void unionSet(Set<Integer> union, Set<Integer> a, Set<Integer> b) {
-        for (int n1 : a) {
-            for (int n2 : b) {
-                union.add(n1 + n2);
-                union.add(n1 - n2);
-                union.add(n1 * n2);
-                if (n2 != 0)
-                    union.add(n1 / n2);
-            }
+     public int solution(int N, int number) {
+               List<Set<Integer>> list = new ArrayList<>();
+        for(int i = 0; i <= 8; i++) {
+            list.add(new HashSet<>());
         }
-    }
-    public int solution(int N, int number) {
-        List<Set<Integer>> setList = new ArrayList<>();
         
-        for (int i = 0; i < 9; i++)
-            setList.add(new HashSet<>()); // 개수 별 해쉬셋
-        setList.get(1).add(N); //1개로 만들 수 있는 건 나 자신뿐
-        if (number == N)
-            return 1;
-        for (int i = 2; i < 9; i++) {
-            for (int j = 1; j <= i / 2; j++) {
-                unionSet(setList.get(i), setList.get(i - j), setList.get(j));
-                unionSet(setList.get(i), setList.get(j), setList.get(i - j));
+        list.get(1).add(N);
+        
+        // 중요: N 한 번 사용해서 number를 만들 수 있는지 체크
+        if(N == number) return 1;
+        
+        for(int i = 2; i <= 8; i++){
+            // 연속된 숫자 만들기 (NN, NNN, NNNN...)
+            String repeat = "";
+            for (int k = 0; k < i; k++){
+                repeat += N;
             }
-            String n = Integer.toString(N);
-            setList.get(i).add(Integer.parseInt(n.repeat(i))); //연속된 숫자 넣기
-            for (int num : setList.get(i))
-                if (num == number)
-                    return i;
+            list.get(i).add(Integer.parseInt(repeat));
+            
+            // 사칙연산으로 새로운 수 만들기
+            for(int j = 1; j < i; j++) {
+                for(int num1 : list.get(j)) {
+                    for(int num2 : list.get(i - j)) {
+                        // 덧셈
+                        list.get(i).add(num1 + num2);
+                        // 뺄셈 (두 방향 모두)
+                        list.get(i).add(num1 - num2);
+                        // 곱셈
+                        list.get(i).add(num1 * num2);
+                        // 나눗셈 (0으로 나누지 않을 때만)
+                        if(num2 != 0) {
+                            list.get(i).add(num1 / num2);
+                        }
+                    }
+                }
+            }
+            
+            // 목표 숫자를 찾았는지 확인
+            if(list.get(i).contains(number)) {
+                return i;
+            }
         }
+        
         return -1;
     }
 }
