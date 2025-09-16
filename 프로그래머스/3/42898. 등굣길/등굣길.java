63c836e58ba34f9b51b578803fced23cc1382da1
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-09-17 00:10:47 +0900

    [level 3] Title: 등굣길, Time: 0.99 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,21 +1,28 @@
+import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int mod = 1000000007;
-        
-        int[][] board = new int[n+1][m+1];
-        for(int i=0; i<puddles.length; i++){
-            board[puddles[i][1]][puddles[i][0]] = -1;
+        int[][] dp = new int[m][n];
+
+        // 물웅덩이 처리
+        for(int[] puddle : puddles) {
+            dp[puddle[0]-1][puddle[1]-1] = -1;
         }
-        board[1][1] = 1;
-        
-        for(int i= 1; i<n+1; i++){
-            for(int j = 1; j<m+1; j++){
-                if(board[i][j] == -1) continue;
-                if (board[i-1][j] > 0) board[i][j] += board[i - 1][j] % mod;
-                if (board[i][j-1] > 0) board[i][j] += board[i][j - 1] % mod;
+
+        dp[0][0] = 1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dp[i][j] == -1) { // 물웅덩이
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if(i > 0) dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
+                if(j > 0) dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
             }
         }
-        
-        return board[n][m] % mod;
+
+        return dp[m-1][n-1] % mod;
     }
 }
