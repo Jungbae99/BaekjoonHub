2ad4af805abc44dcbb77857bc041653f502566ee
전정배 <116333199+Jungbae99@users.noreply.github.com>
2025-09-16 23:12:33 +0900

    [level 3] Title: 정수 삼각형, Time: 14.87 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,20 +1,22 @@
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
-        int tmp[][] = new int[triangle.length][triangle.length];
-
-        tmp[0][0] = triangle[0][0];
-
-        for (int i = 1; i < triangle.length; i++) {
-            tmp[i][0] = tmp[i - 1][0] + triangle[i][0];
-            tmp[i][i] = tmp[i - 1][i - 1] + triangle[i][i];
-
-            for (int j = 1; j <= i - 1; j++) {
-                tmp[i][j] = Math.max(tmp[i - 1][j - 1], tmp[i - 1][j]) + triangle[i][j];
-                answer = Math.max(answer, tmp[i][j]);
+        int[][] dp = new int[triangle.length][triangle.length];
+        
+        dp[0][0] = triangle[0][0];
+        for(int i = 1; i< triangle.length; i++) {
+            dp[i][0] = dp[i-1][0] + triangle[i][0]; // 최좌측
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i]; // 최우측
+            
+            // 예를 들어 i가 4라면 4,5,2,6,5 중에 인덱스 1,2,3만해당
+            for(int j = 1; j< i; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]) +  triangle[i][j];
+                answer = Math.max(answer, dp[i][j]);
             }
+            
+            
         }
-
+        
         return answer;
     }
 }
